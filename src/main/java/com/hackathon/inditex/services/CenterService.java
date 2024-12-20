23a661be6f78f5bc3e37b58a4eee2d772ef3e3d7
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-21 00:45:19 +0100

    required args constructor lombok

--- src/main/java/com/hackathon/inditex/services/CenterService.java
+++ src/main/java/com/hackathon/inditex/services/CenterService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CenterService {
 
     private final CenterRepository centerRepository;
