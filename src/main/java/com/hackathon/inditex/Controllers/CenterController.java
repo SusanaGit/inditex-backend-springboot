6937430af9379d9f6c4a10f2a9fa36e01addbef1
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-21 01:11:40 +0100

    testing GET -> CenterController -> CenterService -> CenterRepository

--- src/main/java/com/hackathon/inditex/Controllers/CenterController.java
+++ src/main/java/com/hackathon/inditex/Controllers/CenterController.java
@@ -1,7 +1,11 @@
 package com.hackathon.inditex.Controllers;
+import com.hackathon.inditex.Entities.Center;
 import com.hackathon.inditex.services.CenterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CenterController {
 
     private final CenterService centerService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    public Center testCenter() {
+        return centerService.testCenterService();
+    }
 
 }
--- src/main/java/com/hackathon/inditex/services/CenterService.java
+++ src/main/java/com/hackathon/inditex/services/CenterService.java
@@ -1,5 +1,6 @@
 package com.hackathon.inditex.services;
 
+import com.hackathon.inditex.Entities.Center;
 import com.hackathon.inditex.repositories.CenterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,9 @@ import org.springframework.stereotype.Service;
 public class CenterService {
 
     private final CenterRepository centerRepository;
+
+    public Center testCenterService() {
+        return centerRepository.findAll().get(0);
+    }
 
 }
