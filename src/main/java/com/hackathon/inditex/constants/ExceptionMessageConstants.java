92e72d2418ae92c7c2f3f0de26104f94255584a2
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-30 02:23:19 +0100

    you can't create an instance of ExceptionMessageContants class

--- src/main/java/com/hackathon/inditex/constants/ExceptionMessageConstants.java
+++ src/main/java/com/hackathon/inditex/constants/ExceptionMessageConstants.java
@@ -5,5 +5,9 @@ public class ExceptionMessageConstants {
     public static final String THERE_IS_ALREADY_A_LOGISTICS_CENTER_IN_THAT_POSITION = "There is already a logistics center in that position.";
     public static final String CURRENT_LOAD_CANNOT_EXCEED_MAX_CAPACITY = "Current load cannot exceed max capacity.";
     public static final String CENTER_NOT_FOUND = "Center not found.";
+    public static final String YOU_CAN_T_CREATE_AN_INSTANCE_OF_THIS_CLASS = "You can't create an instance of this class.";
 
+    private ExceptionMessageConstants() {
+        throw new UnsupportedOperationException(YOU_CAN_T_CREATE_AN_INSTANCE_OF_THIS_CLASS);
+    }
 }
