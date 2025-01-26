efa975e709df3b04091a259077543575493e82ab
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-01-26 18:55:56 +0100

    remove lombok anotation in favour of explicit setters and getters

--- src/main/java/com/hackathon/inditex/entities/Center.java
+++ src/main/java/com/hackathon/inditex/entities/Center.java
@@ -1,9 +1,7 @@
 package com.hackathon.inditex.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "centers", uniqueConstraints = @UniqueConstraint(columnNames = {"latitude", "longitude"}))
 public class Center {
@@ -23,4 +21,60 @@ public class Center {
 
     @Embedded
     private Coordinates coordinates;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(Integer maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public Integer getCurrentLoad() {
+        return currentLoad;
+    }
+
+    public void setCurrentLoad(Integer currentLoad) {
+        this.currentLoad = currentLoad;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
--- src/main/java/com/hackathon/inditex/entities/Coordinates.java
+++ src/main/java/com/hackathon/inditex/entities/Coordinates.java
@@ -2,14 +2,29 @@ package com.hackathon.inditex.entities;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
 public class Coordinates {
+
     private Double latitude;
     private Double longitude;
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
