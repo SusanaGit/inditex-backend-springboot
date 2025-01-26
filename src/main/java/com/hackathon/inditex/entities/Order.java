d1f9478a1ac1ed6d7c7a78e086a872c4e7d07b03
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2025-01-26 18:47:48 +0100

    remove lombok anotation in favour of explicit setters and getters

--- src/main/java/com/hackathon/inditex/entities/Order.java
+++ src/main/java/com/hackathon/inditex/entities/Order.java
@@ -1,9 +1,7 @@
 package com.hackathon.inditex.entities;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -21,5 +19,53 @@ public class Order {
 
     @Embedded
     private Coordinates coordinates;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAssignedCenter() {
+        return assignedCenter;
+    }
+
+    public void setAssignedCenter(String assignedCenter) {
+        this.assignedCenter = assignedCenter;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
 }
 
